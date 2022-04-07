1a8ab736b88961ac7c12f93f49b219a27bac51bd
Justin English <justin.english39@gmail.com>
2022-04-07 15:43:40 -0500

    add april 7th mini-exercises/ beginning work on curriculum

--- external.js
+++ external.js
@@ -1,11 +1,16 @@
 "use.strict";
-console.log('Hello from External JS!')
-var userinput =prompt('please enter your name.');
-console.log('the users name is: ' +userinput);
-alert('you will be charged $3 for every day you rent one movie. Confirm?');
+//console.log('Hello from External JS!')
+//var userinput =prompt('please enter your name.');
+//console.log('the users name is: ' +userinput);
+
+
+
 var price=3
-var bear=prompt('how many days will you be renting Brother Bear for?');
-var mermaid=prompt('how many days will you be renting The Little Mermaid?');
-var hercules=prompt ('how many days will you be renting Hercules?' );
+confirm('you will be charged $'+ (price) +'for every day you rent one movie. Confirm?');
+var bear=parseFloat(prompt('how many days will you be renting Brother Bear for?'));
+var mermaid=parseFloat(prompt('how many days will you be renting The Little Mermaid?'));
+var hercules=parseFloat(prompt('how many days will you be renting Hercules?' ));
 
 alert('you will be charged for ' + (bear+mermaid+hercules)*price);
+
+
