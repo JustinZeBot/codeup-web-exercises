8d7e2075abfef70ec904d21b8cc1867e9d74c108
Justin English <justin.english39@gmail.com>
2022-04-07 09:41:54 -0500

    updated Exercises (updated code) / added instructions on how to make a PBJ sandwich

--- external.js
+++ external.js
@@ -2,3 +2,10 @@
 console.log('Hello from External JS!')
 var userinput =prompt('please enter your name.');
 console.log('the users name is: ' +userinput);
+alert('you will be charged $3 for every day you rent one movie. Confirm?');
+var price=3
+var bear=prompt('how many days will you be renting Brother Bear for?');
+var mermaid=prompt('how many days will you be renting The Little Mermaid?');
+var hercules=prompt ('how many days will you be renting Hercules?' );
+
+alert('you will be charged for ' + (bear+mermaid+hercules)*price);
