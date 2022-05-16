d281eac1c3ab5f80d2a653466aa68b413e528e34
Justin English <justin.english39@gmail.com>
2022-05-16 14:53:53 -0500

    Jquery Added, It works but is not pretty

---
+++ js/JqueryExercises.js
@@ -0,0 +1,14 @@
+"use strict";
+
+// sanity checker
+// window.onclick = function() {
+//     alert('click!');
+// }
+
+//======
+
+
+});
+console.log('sanitycheck')
+
+$('.List1').css('background-color', 'yellow');
